2a754deca7783281967aba7c0a43652fee2adba1
Xairl <104131515+Xairl@users.noreply.github.com>
2023-06-12 14:56:21 +0200

    Addition of Motor

---
+++ Board Codes/Motor/Motor_v1/src/MotorHandler.cpp
@@ -0,0 +1,72 @@
+#include "MotorHandler.h"
+
+MotorHandler::MotorHandler(MCP_CAN& CAN, int canId, float _kp, float _kd) : gim8115(canId, _kp, _kd), canHandler(CAN)
+{
+  baseVelocity = 0;
+  baseTorque = 0;
+}
+
+void MotorHandler::moveMotor(float newPos)
+{
+  // sending new position to motor library using predefined velocity and torque
+  normalSet(canHandler, newPos, baseVelocity, baseTorque);
+}
+
+void MotorHandler::exitMotorMode()
+{
+  exitMotormode(canHandler);
+}
+void MotorHandler::enterMotorMode()
+{
+  setMotormode(canHandler);
+}
+
+void MotorHandler::setTorqueMode(float tarTor)
+{
+  // Call the private functions to build the individual packages
+  float *torPackage = buildTorquePackage(tarTor);
+
+  //----------------------------------------------------------------------------//
+  // Sending data//
+  unsigned char buf[8];
+  buf[0] = 0;
+  buf[1] = 0;
+  buf[2] = 0;
+  buf[3] = 0;
+  buf[4] = 0;
+  buf[5] = 0;
+  buf[6] = torPackage[0];
+  buf[7] = torPackage[1];
+
+  byte sndStat = canHandler.sendMsgBuf(getId(), 0, 8, buf);
+
+  if (sndStat == CAN_OK)
+  {
+    // Serial.println("Message Sent Successfully!");
+  }
+  else
+  {
+    // Serial.println("Error Sending Message!");
+  }
+
+}
+
+void MotorHandler::resetPosition()
+{
+  setZero(canHandler);
+}
+
+void MotorHandler::setPositionFull(float newPos, float velocity, float torque)
+{
+  normalSet(canHandler, newPos, velocity, torque);
+}
+
+void MotorHandler::setBaseVelocity(float vel)
+{
+  baseVelocity = vel;
+}
+
+void MotorHandler::setBaseTorque(float trq)
+{
+  baseTorque = trq;
+}
---
+++ Board Codes/Motor/Motor_v1/src/MotorHandler.h
@@ -0,0 +1,26 @@
+#ifndef MOTOR_HANDLER_H
+#define MOTOR_HANDLER_H
+
+#include <gim8115.h>
+
+class MotorHandler : public gim8115
+{
+private:
+  MCP_CAN &canHandler;
+  float baseVelocity;
+  float baseTorque;
+
+public:
+  MotorHandler(MCP_CAN &CAN, int canId, float _kp, float _kd); // Constructor - sets the motor number
+  void moveMotor(float newPos);                                // Move the motor to the specified position using base velocity and torque
+  // TODO: think about new name to function below to avoid misunderstading with parent class
+  void exitMotorMode();                                             // sends command to leave motor mode
+  void enterMotorMode();                                            // sends command to enter motor mode
+  void setTorqueMode(float tarTor);                                 // Set the motor to torque mode
+  void resetPosition();                                             // Reset the motor position to zero
+  void setPositionFull(float newPos, float velocity, float torque); // Set normal mode function of the parent class
+  void setBaseVelocity(float baseVelocity);                         // define base velocity for movemotor
+  void setBaseTorque(float baseTorque);                             // define base torque for movemotor
+};
+
+#endif
